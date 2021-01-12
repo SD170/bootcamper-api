72935ac52de21ebddf8a4d503d26d688e55b5419
SD170 <duttasaswata7@gmail.com>
2021-01-12 17:53:41 +0530

    data added

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -22,5 +27,13 @@ app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+//handle unhandled PromeseRejection
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`.red);
+    
+    //Close Server & exit process
+    server.close(()=> process.exit(1));
+})
 
